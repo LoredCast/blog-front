588ad8273892cb292884c79c49a2e5a5e758932d
Manuel <loredcastmail@gmail.com>
2025-02-12 10:48:28 +0100

    ssr test

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -3,7 +3,7 @@ import getDirectusInstance from '$lib/directus';
 import { readItems } from '@directus/sdk';
 export async function load({ fetch }) {
     const directus = getDirectusInstance(fetch);
-    console.log(fetch("https://directus.manuelunterriker.de/items/articles"))
+    console.log(await fetch("https://directus.manuelunterriker.de/items/articles"))
     return {
         articles: await directus.request(readItems('articles', {
             fields: ['url', 'title', 'date_created', { author: ['name'] }, 'summary'],
